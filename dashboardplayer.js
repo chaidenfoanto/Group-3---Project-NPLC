53ef8547c34da73c5d9d3ebfb0aa547d36ae24ef
alicialisal <ajuanita@student.ciputra.ac.id>
2024-05-25 19:15:44 +0800

    responsive dashboard player

--- dashboardplayer.js
+++ dashboardplayer.js
@@ -1,24 +1,18 @@
 $(document).ready(function() {
-const sidebar = $(".sidebar");
-const toggleBtn = $("#toggle-btn");
-const logo = $(".logo_details .logo").eq(1); // Select the second logo
-const searchBtn = $(".bx-search");
+    $(".sidebar").load("sidebar.html", function() {
+        const toggleBtn = $("#toggle-btn");
+        const logo = $(".logo_details .logo").eq(1); // Select the second logo
+        toggleBtn.on("click", function() {
+            $(".sidebar").toggleClass("open");
+            menuBtnChange();
+        });
 
-toggleBtn.on("click", function() {
-    sidebar.toggleClass("open");
-    menuBtnChange();
-});
-    
-    searchBtn.on("click", function() {
-        sidebar.toggleClass("open");
-        menuBtnChange();
+        function menuBtnChange() {
+            if (sidebar.hasClass("open")) {
+                logo.hide();
+            } else {
+                logo.show();
+            }
+        }
     });
-    
-    function menuBtnChange() {
-        if (sidebar.hasClass("open")) {
-            logo.hide();
-        } else {
-            logo.show();
-        }
-        }
 });
