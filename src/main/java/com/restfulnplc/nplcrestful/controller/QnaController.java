625a1477e58573ff6a1f7c2bb2183d90dc430389
Gibekkk <gfoucauld@student.ciputra.ac.id>
2024-06-18 17:36:20 +0800

    Update QnaController.java

--- src/main/java/com/restfulnplc/nplcrestful/controller/QnaController.java
+++ src/main/java/com/restfulnplc/nplcrestful/controller/QnaController.java
@@ -1,7 +1,23 @@
 package com.restfulnplc.nplcrestful.controller;
 
-import com.restfulnplc.nplcrestful.dto.QnaPlayersDTO;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.restfulnplc.nplcrestful.dto.QnaPanitiaDTO;
+import com.restfulnplc.nplcrestful.dto.QnaPlayersDTO;
 import com.restfulnplc.nplcrestful.model.Qna;
 import com.restfulnplc.nplcrestful.service.LoginService;
 import com.restfulnplc.nplcrestful.service.QnaService;
@@ -10,14 +26,6 @@ import com.restfulnplc.nplcrestful.util.HTTPCode;
 import com.restfulnplc.nplcrestful.util.Response;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @CrossOrigin
