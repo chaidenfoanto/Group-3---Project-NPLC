a9e6fb594ff0eee3b07db45b07cb6cbe7c43b1fb
alicialisal <ajuanita@student.ciputra.ac.id>
2024-06-20 16:12:39 +0800

    Update leaderboardhod.js

--- leaderboardhod.js
+++ leaderboardhod.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $(".sidebar").load("sidebarhod.html", function() {
+    $(".sidebar").load("sidebarHod.html", function() {
         const toggleBtn = $("#toggle-btn, #burger-btn");
         const logo = $(".logo_details .logo").eq(1); // Select the second logo
         toggleBtn.on("click", function() {
