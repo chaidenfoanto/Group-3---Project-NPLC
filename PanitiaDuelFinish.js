d5ce0ac50b39c77aeadfa8579316a04781ea8dbd
DerickNorlan <dnorlan01@student.ciputra.ac.id>
2024-06-21 16:50:46 +0800

    coba sidebar

--- PanitiaDuelFinish.js
+++ PanitiaDuelFinish.js
@@ -33,20 +33,20 @@ function getTime() {
 getTime();
 
 $(document).ready(function () {
-  $('.sidebar').load('sidebarpanitia.html', function () {
-    const toggleBtn = $('#toggle-btn, #burger-btn');
-    const logo = $('.logo_details .logo').eq(1); // Select the second logo
-    toggleBtn.on('click', function () {
-      $('.sidebar').toggleClass('open');
-      menuBtnChange();
+  $(".sidebar").load("sidebarpanitia.html", function() {
+    const toggleBtn = $("#toggle-btn, #burger-btn");
+    const logo = $(".logo_details .logo").eq(1); // Select the second logo
+    toggleBtn.on("click", function() {
+        $(".sidebar").toggleClass("open");
+        menuBtnChange();
     });
 
     function menuBtnChange() {
-      if (sidebar.hasClass('open')) {
-        logo.hide();
-      } else {
-        logo.show();
-      }
+        if ($(".sidebar").hasClass("open")) {
+            logo.hide();
+        } else {
+            logo.show();
+        }
     }
   });
 
