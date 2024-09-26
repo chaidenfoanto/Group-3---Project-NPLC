b7e9bd7bed863bea761a42c29e5e82c5872e6783
chaidenfoanto <chaiden.foanto@gmail.com>
2024-09-26 12:40:11 +0800

    tes

--- front-end/editProfile.js
+++ front-end/editProfile.js
@@ -1,4 +1,32 @@
 $(document).ready(function() {
+    const domain = 'http://localhost:8080/';
+    $('.sidebar').load('sidebarpanitia.html', function () {
+      const toggleBtn = $('#toggle-btn');
+      const logo = $('.logo_details .logo').eq(1);
+      toggleBtn.on('click', function () {
+        $('.sidebar').toggleClass('open');
+        menuBtnChange();
+      });
+  
+      function menuBtnChange() {
+        if (sidebar.hasClass('open')) {
+          logo.hide();
+        } else {
+          logo.show();
+        }
+      }
+    });
+
+    $(document).on('click', function (e) {
+        if (!$(e.target).closest('.sidebar, #toggle-btn').length) {
+          closeSidebar();
+        }
+      });
+
+      function closeSidebar() {
+        $('.sidebar').removeClass('open');
+        // $('.main-content').removeClass('shift');
+      }
 
     $('.inner-container input, .inner-container textarea, .inner-container select').each(function() {
         // Check if the input is not empty on page load
